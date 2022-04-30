d17b3a8812db75fbb6d51d51225a7e5194369372
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-30 16:41:30 +0100

    Began work on point system & game manager

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] int pointValue;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Increase player score");
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] int defaultScoreRate = 5;
+    [SerializeField] float timeIncrements = 0.25f;
+
+    private int score = 0;
+    private bool isGamePlaying = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(IncreaseScoreAuto());
+    }
+
+    private IEnumerator IncreaseScoreAuto()
+    {
+        while (isGamePlaying)
+        {
+            //Debug.Log("Score: " + score);
+            score += defaultScoreRate;
+            yield return new WaitForSeconds(timeIncrements);
+        }
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        //Debug.Log("Score: " + score);
+    }
+
+}
