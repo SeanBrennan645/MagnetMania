11c68e62c5ff69a03d7a2d4eebaa2c86c2863a8f
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-30 13:51:10 +0100

    Added simple version of player movement
    Player can press spacebar or left click to move to either the left or right side of the screen

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float maxDist = 0.0f;
+    [SerializeField][Range(0.0f, 10.0f)] float maxSpeed = 10.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] float chargeAmount = 0.0f; //Can make private in future
+
+    private bool onLeft = true;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckPlayerInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        if(onLeft && isMoving)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+            if(transform.position.x >= maxDist)
+            {
+                isMoving = false;
+            }
+        }
+        else if(!onLeft && isMoving)
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+            if(transform.position.x <= -maxDist)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    private void CheckPlayerInput()
+    {
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && !isMoving)
+        {
+            isMoving = true;
+            onLeft = !onLeft;
+        }
+    }
+}
