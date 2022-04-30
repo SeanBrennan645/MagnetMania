52a0d44c9fd7df0e77530e565a623416d2ed36c3
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-30 14:59:28 +0100

    Added a faller script
    Created a simple script to move objects down, once objects reach out of bounds they are set to unactive rather than being deleted so we can make use of object pooling

---
+++ Assets/Scripts/ObjectFaller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFaller : MonoBehaviour
+{
+    [SerializeField] float fallSpeed = 1.0f;
+
+    void Update()
+    {
+        transform.position += transform.up * -fallSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bounds")
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
